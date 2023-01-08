bdb3e0b3065445891f0fa6fc87c42cd6591982c0
Nate Canterbury <Nateclsu@outlook.com>
2023-01-07 19:45:55 -0600

    update: form updated with netlify

--- js/main.js
+++ js/main.js
@@ -617,14 +617,15 @@
 
     /*----------- Contact -----------*/
 
-    $('.contact-form').on('submit', function(event) {
+    /* $('.contact-form').on('submit', function(event) {
       var form = $(this);
       var submitBtn = form.find('#contact-submit');
       var submitBtnText = submitBtn.text();
       var feedbackEl = form.find('.contact-feedback');
       event.preventDefault();
+      */
       // Waiting for the response from the server
-      submitBtn.html('Wait...').addClass('wait').prop('disabled', true);
+      /* submitBtn.html('Wait...').addClass('wait').prop('disabled', true);
       setTimeout(function() {
         // Posts the Form's data to the server using Ajax
         $.ajax({
@@ -632,21 +633,23 @@
             type: 'POST',
             data: form.serialize(),
           })
+      */
           // Getting a response from the server
-          .done(function(response) {
+      //    .done(function(response) {
             // If the PHP file succeed sending the message
-            if (response == 'success') {
+      //    if (response == 'success') {
               // Feedback to the user
-              submitBtn.removeClass('wait').html('Success').addClass('success');
+              /* submitBtn.removeClass('wait').html('Success').addClass('success');
               feedbackEl.addClass('success').html('Thank you for your message. It has been sent.').fadeIn(200);
               setTimeout(function() {
                 submitBtn.html(submitBtnText).removeClass('success').prop('disabled', false);
                 feedbackEl.fadeOut(200).removeClass('success').html('');
               }, 6000);
+              */
               // Clears the Form
-              form[0].reset();
+        //    form[0].reset();
               // If something is wrong
-            } else {
+          /*  } else {
               // Feedback to the user
               console.log(response);
               submitBtn.removeClass('wait').html('Error').addClass('error');
@@ -659,9 +662,11 @@
           });
       }, 1000);
     });
-
+            
   });
-  $(window).on('load', function() {
+  $(window).on('load', function() { 
+          */
+
     /*----------- Preloader -----------*/
 
     $('.preloader-icon').fadeOut(400);
